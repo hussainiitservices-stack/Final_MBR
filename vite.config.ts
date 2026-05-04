b8044f9c67e515d9e5a9e068c02e0a3a0a5ec4f6
Hussain-2002 <Hussainmesharwala@gmail.com>
2026-05-04 11:11:53 +0530

    Hussain

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 
 // https://vitejs.dev/config/
@@ -25,7 +26,15 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 80 },
+      webp: { quality: 80 },
+      avif: { quality: 70 },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
